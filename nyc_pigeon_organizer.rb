99a8283349538a9cea97e8ac8a39221116b7881f
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-28 05:20:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,24 +4,30 @@ def nyc_pigeon_organizer(data)
   pigeon_names = []
   
   data.each do | trait, property |
+    # iterate over data set which contains traits with properties
     property.each do | attribute, array |
+      # iterate over property which contains attribute and array
       array.each do | name |
+        # iterate over the array, taking each name and checking to see if already 
+        # included in pigeon_names, if not then shovels into the pigeon_names array
         pigeon_names << name if !pigeon_names.include?(name)
       end
     end
   end
   
   pigeon_names.each do | name |
+    # for each of the names in pigeon_names array, 
+    # sets pigeon_list hash key to name with value to zero
     pigeon_list[name] = 0
   end
     
-  data.each do | trait, property |
-    property.each do | attribute, array |
-      array.each do | name |
-        pigeon_names << name if !pigeon_names.include?(name)
-      end
-    end
-  end
+  # data.each do | trait, property |
+  #   property.each do | attribute, array |
+  #     array.each do | name |
+  #       pigeon_names << name if !pigeon_names.include?(name)
+  #     end
+  #   end
+  # end
   
   pigeon_list
 end
