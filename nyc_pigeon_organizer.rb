c1c0297a25f21d05becf99a2a03ab5e9ebdf3957
Leigha De La Rosa <cdversiontwo@gmail.com>
2019-11-28 05:25:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,6 +21,11 @@ def nyc_pigeon_organizer(data)
     pigeon_list[name] = 0
   end
     
+  p pigeon_list
+  
+  pigeon_list
+end
+
   # data.each do | trait, property |
   #   property.each do | attribute, array |
   #     array.each do | name |
@@ -28,43 +33,8 @@ def nyc_pigeon_organizer(data)
   #     end
   #   end
   # end
-  
-  pigeon_list
-end
 
 
 
-# def directors_totals(nds)
-#   result = {
-#     # :director => total
-#   }
 
-#   # loop through each directors_database[:name] 
-#   i = 0 
-#   while i < directors_database.length do 
-    
-#     source_array = directors_database[i][:movies]
-#     director = directors_database[i][:name]
-    
-#     result[director] = 0 
-#     puts result
 
-# # iterate through movies array to grab value of :worldwide_gross key, sum up values
-   
-#     total = 0 
-#     j = 0 
-#     while j < source_array.length do
-#       total += source_array[j][:worldwide_gross]
-#       j += 1 
-#     end
-    
-#     puts total
-#     result[director] = total
-    
-#     i += 1
-#     end
-  
-# # return hash of { :name => total }
-
-# return result
-# end  
